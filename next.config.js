e33103d090098025cbfc80b3a8e7d74d97d078e3
Oleksij Kozakovskyj <oleksij.kozakovskyj@gmail.com>
2023-03-13 11:24:03 +0200

    fix prod

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
